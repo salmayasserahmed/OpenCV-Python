6abca43766d27d13b4184cf2a7d26a35950feb52
Salma Yasser <70496209+salmayasserahmed@users.noreply.github.com>
2021-09-11 20:44:38 +0200

    Update ReadImage.py

--- Basics/ReadImage.py
+++ Basics/ReadImage.py
@@ -1,1 +1,5 @@
+import cv2
+img = cv2.imread('images/Cup.jfif') #Matrix of Image Pixels
+cv2.imshow("Cup",img) #Show Image os a new window that takes two parameter, 1. Window Name, 2. Actual Matrix of Pixels.
+cv2.waitKey(0) #Keyboard Binding Function. It waits for specific time in miliseconds for a key to be pressed.
 
